018fdbb33e9c467456afa20795e7e96cdcc6f7d2
Leandro Pacheco <contact@leandropacheco.com>
2017-11-18 01:16:20 +0100

    new binaries for benchmarking
    these programs seem to show starvation happening in the following
    case:
    - echo_server_broadcast
    - 2 clients echo_client_bench
    
    A single client works fine, but with 2, the numbers get weird.

---
+++ src/bin/echo_client_bench.rs
@@ -0,0 +1,127 @@
+extern crate histogram;
+extern crate sevent;
+extern crate rand;
+extern crate mio;
+
+use histogram::Histogram;
+
+use sevent::ext::VecExt;
+
+use std::time::Duration;
+use std::time::SystemTime;
+use std::collections::HashSet;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use mio::net::TcpStream;
+
+
+struct Echo {
+    stats: Rc<RefCell<Histogram>>,
+    pending: Rc<RefCell<HashSet<u64>>>,
+}
+
+pub fn duration_as_usecs(d: Duration) -> u64 {
+    d.as_secs()*1000000 + d.subsec_nanos() as u64/1000
+}
+
+impl sevent::ConnectionHandler for Echo {
+    fn on_read(&mut self, id: usize, buf: &mut Vec<u8>) {
+        let mut pending = self.pending.borrow_mut();
+        for msg in buf.drain_frames_bincode() {
+            let (msg_id, send_time): (u64, SystemTime) = msg.unwrap();
+            if pending.remove(&msg_id) {
+                let now = SystemTime::now();
+                let lat = now.duration_since(send_time).unwrap();
+                let mut stats = self.stats.borrow_mut();
+                stats.increment(duration_as_usecs(lat)).unwrap();
+                // send another msg
+                sevent::connection_write(id, |wbuf| {
+                    let msg: (u64, _) = (rand::random(), SystemTime::now());
+                    pending.insert(msg.0);
+                    wbuf.put_frame_bincode(&msg).unwrap();
+                });
+            }
+        }
+    }
+
+    fn on_disconnect(&mut self, id: usize, err: Option<sevent::Error>) {
+        println!("connection {} disconneted: {:?}", id, err);
+    }
+}
+
+fn print_stats(hist: &Histogram) {
+    if hist.entries() > 0 {
+        println!("tput: {} op/sec\tavg_lat: {} usec\tmax_lat: {}\tstd_dev: {}\n\
+                  \tmedian: {}\t95th: {}\t99th: {}\t99.9th: {}",
+                 hist.entries(),
+                 hist.mean().unwrap(),
+                 hist.maximum().unwrap(),
+                 hist.stddev().unwrap(),
+                 hist.percentile(50.0).unwrap(),
+                 hist.percentile(95.0).unwrap(),
+                 hist.percentile(99.0).unwrap(),
+                 hist.percentile(99.9).unwrap());
+    } else {
+        println!("tput: 0 op/sec");
+    }
+}
+
+struct PrintStats {
+    stats: Rc<RefCell<Histogram>>,
+}
+
+impl sevent::TimeoutHandler for PrintStats {
+    fn on_timeout(self: Box<Self>) {
+        {
+            let mut stats = self.stats.borrow_mut();
+            print_stats(&*stats);
+            stats.clear();
+        }
+        sevent::set_timeout(Duration::from_secs(1), *self).unwrap();
+    }
+}
+
+
+fn main() {
+    sevent::run_evloop(|| {
+
+        let addr = "127.0.0.1:10000".parse().unwrap();
+
+        let outstanding: usize = std::env::args().nth(1).unwrap().parse().unwrap();
+
+        let pending = Rc::new(RefCell::new(HashSet::new()));
+
+        // print statistics
+        let stats = Rc::new(RefCell::new(Histogram::new()));
+        sevent::set_timeout(Duration::from_secs(1), PrintStats { stats: stats.clone() })?;
+
+        let echo = Echo {
+            pending: pending.clone(),
+            stats,
+        };
+
+        sevent::add_connect(addr, move |addr, res: Result<TcpStream, _>| {
+            match res {
+                Ok(stream) => {
+                    stream.set_nodelay(true);
+                    println!("connected to {:?}!", addr);
+                    let conn_id = sevent::add_connection(stream, echo).unwrap();
+                    for _ in 0..outstanding {
+                        sevent::connection_write(conn_id, |wbuf| {
+                            let id: u64 = rand::random();
+                            let msg = (id, SystemTime::now());
+                            pending.borrow_mut().insert(id);
+                            wbuf.put_frame_bincode(&msg).unwrap();
+                        })
+                    }
+                }
+                Err(err) => {
+                    println!("error connecting to {:?}: {:?}", addr, err);
+                    sevent::shutdown();
+                }
+            }
+        }).unwrap();
+        Ok(())
+    }).unwrap();
+}
---
+++ src/bin/echo_server_broadcast.rs
@@ -0,0 +1,57 @@
+extern crate sevent;
+extern crate mio;
+
+use std::time::SystemTime;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashSet;
+
+use sevent::ext::VecExt;
+
+struct Echo {
+    connections: Rc<RefCell<HashSet<usize>>>,
+}
+
+impl sevent::ConnectionHandler for Echo {
+    fn on_read(&mut self, _id: usize, buf: &mut Vec<u8>) {
+        let connections = self.connections.borrow();
+        for msg in buf.drain_frames_bincode() {
+            let msg: (u64, SystemTime) = msg.unwrap();
+            for id in connections.iter() {
+                sevent::connection_write(*id, |wbuf| {
+                    wbuf.put_frame_bincode(&msg).unwrap();
+                });
+            }
+        }
+    }
+
+    fn on_disconnect(&mut self, id: usize, err: Option<sevent::Error>) {
+        self.connections.borrow_mut().remove(&id);
+        println!("connection {} disconnected: {:?}", id, err);
+    }
+}
+
+fn main() {
+    sevent::run_evloop(|| {
+        let addr = "127.0.0.1:10000".parse().unwrap();
+
+        let connections: Rc<RefCell<HashSet<usize>>> = Rc::default();
+
+        let listener = mio::net::TcpListener::bind(&addr).unwrap();
+
+        let id = sevent::add_listener(listener, move |res| {
+            match res {
+                Ok((stream, addr)) => {
+                    let id = sevent::add_connection(stream, Echo {
+                        connections: connections.clone(),
+                    }).unwrap();
+                    connections.borrow_mut().insert(id);
+                    println!("new connection {} from {:?}", id, addr);
+                }
+                Err(err) => panic!("{:?}", err),
+            }
+        }).unwrap();
+        println!("listener with id {:?}", id);
+        Ok(())
+    }).unwrap();
+}
